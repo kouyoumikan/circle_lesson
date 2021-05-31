30496f84047635555fdce7caa87d0294a8905446
kouyoumikan <kouyoumikan136@gmail.com>
2021-05-31 16:56:54 +0900

    コミット　テスト

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,93 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MaterialApp(
+  home: NinjaCard(),
+));
+
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text("Ninja ID Card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0, // ドロップシャドウを表示しない
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/inu_ninja.png"),
+                radius: 40.0, // 円のサイズ設定
+              ),
+            ),
+            Divider( // 仕切り線
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              "Name",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0), // 文字下にボックス分のスペース開ける
+            Text(
+              "Chun-Li",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0), // 文字下にボックス分のスペース開ける
+            Text(
+              "CURRENT NINJA LEVEL",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0), // 文字下にボックス分のスペース開ける
+            Text(
+              "8",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0), // 文字下にボックス分のスペース開ける
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0), // アイコン横にボックス分のスペース開ける
+                Text(
+                  "chun.li@thenetninja.co.jp",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
+
 
